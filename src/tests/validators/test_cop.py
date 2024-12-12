cfe0f3b207ee13b40c8f37fba32f920a01615a31
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2024-12-12 20:48:43 +0100

    add cop validation

---
+++ src/tests/validators/test_cop.py
@@ -0,0 +1,17 @@
+import unittest
+
+from validators.cop import validate_cop
+
+
+class TestCopValidator(unittest.TestCase):
+    def case_valid(self, cop: str, expected=None):
+        output = validate_cop(cop)
+
+        if expected:
+            self.assertEqual(output, expected)
+
+    def test_validate_cop(self):
+        # valid:
+        self.case_valid("BOT", "BOT")
+        self.case_valid("DIXAT", "DIXAT")
+        self.case_valid(" ELDAR ", "ELDAR")
---
+++ src/validators/cop.py
@@ -0,0 +1,2 @@
+def validate_cop(cop: str):
+    return cop.strip().upper()
