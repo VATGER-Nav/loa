12180a5f608a11dd8607dd0936197c95bef56a3a
Robin Gloster <mail@glob.in>
2025-04-17 13:21:21 +0200

    flake: update

--- flake.nix
+++ flake.nix
@@ -19,76 +19,110 @@
 
     pyproject-build-systems = {
       url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        nixpkgs.follows = "nixpkgs";
+      };
     };
   };
 
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
 
-      perSystem =
-        { pkgs, lib, ... }:
-        let
-          workspace = inputs.uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+      perSystem = {
+        pkgs,
+        lib,
+        ...
+      }: let
+        workspace = inputs.uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
 
-          overlay = workspace.mkPyprojectOverlay {
-            sourcePreference = "wheel";
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+        };
+
+        python = pkgs.python313;
+
+        pythonSet =
+          (pkgs.callPackage inputs.pyproject-nix.build.packages {
+            inherit python;
+          })
+          .overrideScope
+          (
+            lib.composeManyExtensions [
+              inputs.pyproject-build-systems.overlays.default
+              overlay
+            ]
+          );
+      in {
+        formatter = pkgs.nixfmt-rfc-style;
+
+        packages = {
+          default = pythonSet.mkVirtualEnv "loa-env" workspace.deps.default;
+        };
+
+        devShells = {
+          impure = pkgs.mkShell {
+            packages = [
+              python
+              pkgs.uv
+            ];
+            shellHook = ''
+              unset PYTHONPATH
+            '';
           };
 
-          python = pkgs.python313;
+          default = let
+            editableOverlay = workspace.mkEditablePyprojectOverlay {
+              root = "$REPO_ROOT";
+            };
+            editablePythonSet = pythonSet.overrideScope (
+              lib.composeManyExtensions [
+                editableOverlay
 
-          pythonSet =
-            (pkgs.callPackage inputs.pyproject-nix.build.packages {
-              inherit python;
-            }).overrideScope
-              (
-                lib.composeManyExtensions [
-                  inputs.pyproject-build-systems.overlays.default
-                  overlay
-                ]
-              );
-        in
-        {
-          formatter = pkgs.nixfmt-rfc-style;
-
-          packages = {
-            default = pythonSet.mkVirtualEnv "loa-env" workspace.deps.default;
-          };
-
-          devShells = {
-            impure = pkgs.mkShell {
+                # Apply fixups for building an editable package of your workspace packages
+                (final: prev: {
+                  loa = prev.loa.overrideAttrs (old: {
+                    # Hatchling (our build system) has a dependency on the `editables` package when building editables.
+                    #
+                    # In normal Python flows this dependency is dynamically handled, and doesn't need to be explicitly declared.
+                    # This behaviour is documented in PEP-660.
+                    #
+                    # With Nix the dependency needs to be explicitly declared.
+                    nativeBuildInputs =
+                      old.nativeBuildInputs
+                      ++ final.resolveBuildSystem {
+                        editables = [];
+                      };
+                  });
+                })
+              ]
+            );
+            virtualenv = editablePythonSet.mkVirtualEnv "loa-dev-env" workspace.deps.all;
+          in
+            pkgs.mkShell {
               packages = [
-                python
+                virtualenv
                 pkgs.uv
               ];
+              env = {
+                # Don't create venv using uv
+                UV_NO_SYNC = "1";
+
+                # Force uv to use Python interpreter from venv
+                UV_PYTHON = "${virtualenv}/bin/python";
+
+                # Prevent uv from downloading managed Python's
+                UV_PYTHON_DOWNLOADS = "never";
+              };
+
               shellHook = ''
                 unset PYTHONPATH
+                export REPO_ROOT=$(git rev-parse --show-toplevel)
               '';
             };
-
-            default =
-              let
-                editableOverlay = workspace.mkEditablePyprojectOverlay {
-                  root = "$REPO_ROOT";
-                };
-                editablePythonSet = pythonSet.overrideScope editableOverlay;
-                virtualenv = editablePythonSet.mkVirtualEnv "loa-dev-env" workspace.deps.all;
-              in
-              pkgs.mkShell {
-                packages = [
-                  virtualenv
-                  pkgs.uv
-                ];
-                shellHook = ''
-                  unset PYTHONPATH
-                  export REPO_ROOT=$(git rev-parse --show-toplevel)
-                '';
-              };
-          };
         };
+      };
     };
 }
